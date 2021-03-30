32d4e5586985c9d30fcdd3c60ef226a0f647b359
Rodrigo Bindewald <nevesrnb@gmail.com>
2021-03-30 16:36:09 -0300

    initial commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const server = express()
+const routes = require("./routes")
+
+//ejs engine
+server.set('view engine', 'ejs')
+
+//enable static files 
+server.use(express.static("public"))
+
+//routes
+server.use(routes)
+
+server.listen(3000, () => console.log('rodando'))
