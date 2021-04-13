adccca355cb41c1b0d8c31ee32fa604d176ecbe1
Rodrigo Bindewald <nevesrnb@gmail.com>
2021-04-13 16:00:48 -0300

    aula 02

--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@ const express = require("express")
 const server = express()
 const routes = require("./routes")
 
-//ejs engine
+//ejs template engine
 server.set('view engine', 'ejs')
 
-//enable static files 
+// habilitar arquivos statics
 server.use(express.static("public"))
 
-//routes
+// usando req.body
+server.use(express.urlencoded({ extended: true }))
+
+// routes
 server.use(routes)
 
 server.listen(3000, () => console.log('rodando'))
